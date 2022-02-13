3e50b4eaa5901ce249ab6aa9e3a1e4d2c45bacee
Lukypop <91247831+Lukypop@users.noreply.github.com>
2022-02-13 21:58:04 +0100

    Pushing P

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE)
+pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
+P1 = False
+P2 = False
+Game = False
+Lock = False
+def main():
+    global Game
+    basic.pause(randint(3000, 10000))
+    if not Lock:
+        basic.show_icon(IconNames.HEART, 0)
+        Game = True
+        music.play_tone(Note.C, 1500)    
+def check():
+    global P1, P2, Lock, Game
+    P1 = input.pin_is_pressed(TouchPin.P1) 
+    P2 = input.pin_is_pressed(TouchPin.P2) 
+    if not Lock:
+        if Game: 
+            if P1 and P2:
+                basic.show_string("R")
+                Lock = True
+            elif P1:
+                basic.show_string("1")
+                Lock = True
+            elif P2:
+                basic.show_string("2")
+                Lock = True
+        else:
+            if P1 and P2:
+                basic.show_string("C")
+                Lock = True
+            elif P1:
+                basic.show_string("B")
+                Lock = True
+            elif P2:
+                basic.show_string("A")
+                Lock = True
+    else:
+        basic.pause(3000)
+        basic.clear_screen()
+        Game = False
+        Lock = False
+        control.in_background(main)
+basic.forever(check)
+control.in_background(main)
--- main.ts
+++ main.ts
@@ -1,3 +1,55 @@
-basic.forever(function on_forever() {
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+let P1 = false
+let P2 = false
+let Game = false
+let Lock = false
+function main() {
+    
+    basic.pause(randint(3000, 10000))
+    if (!Lock) {
+        basic.showIcon(IconNames.Heart, 0)
+        Game = true
+        music.playTone(Note.C, 1500)
+    }
+    
+}
+
+basic.forever(function check() {
+    
+    P1 = input.pinIsPressed(TouchPin.P1)
+    P2 = input.pinIsPressed(TouchPin.P2)
+    if (!Lock) {
+        if (Game) {
+            if (P1 && P2) {
+                basic.showString("R")
+                Lock = true
+            } else if (P1) {
+                basic.showString("1")
+                Lock = true
+            } else if (P2) {
+                basic.showString("2")
+                Lock = true
+            }
+            
+        } else if (P1 && P2) {
+            basic.showString("C")
+            Lock = true
+        } else if (P1) {
+            basic.showString("B")
+            Lock = true
+        } else if (P2) {
+            basic.showString("A")
+            Lock = true
+        }
+        
+    } else {
+        basic.pause(3000)
+        basic.clearScreen()
+        Game = false
+        Lock = false
+        control.inBackground(main)
+    }
     
 })
+control.inBackground(main)
